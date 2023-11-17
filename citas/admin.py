de1e1f40d72aa8fbc348a433c05f84203ecdb692
auroranavas <navasaurora@outlook.com>
2023-11-17 18:06:11 +0100

    fix/feat: arreglo en los modelos y vista de admin

--- citas/admin.py
+++ citas/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Servicio, Especialista, Invitado, Cita
+
+
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "descripcion"]
+    search_fields = ["nombre"]
+
+
+admin.site.register(Servicio, ServicioAdmin)
+
+
+class EspecialistaAdmin(admin.ModelAdmin):
+    list_display = ["nombre"]
+    filter_horizontal = ["especialidades"]
+
+
+admin.site.register(Especialista, EspecialistaAdmin)
+
+
+class InvitadoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "email", "telefono")
+
+    search_fields = (
+        "nombre",
+        "email",
+        "telefono",
+    )
+
+
+admin.site.register(Invitado, InvitadoAdmin)
+
+
+class CitaAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "invitado", "servicio", "especialista", "fecha")
+
+    search_fields = (
+        "usuario",
+        "invitado",
+        "servicio",
+        "especialista",
+        "fecha",
+    )
+    date_hierarchy = "fecha"
+
+
+admin.site.register(Cita, CitaAdmin)
