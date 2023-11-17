ab1498ff1325cea60c4653b86e3cd95968c23ebd
Sammy <samrodman@alum.us.es>
2023-11-17 12:47:07 +0100

    feat: created profile basic page  + Home logos

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home
+from . import views
 
 urlpatterns = [
     path("", home, name="home"),
+    path("perfil/", views.perfil, name="perfil"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home/home.html")
+
+
+def perfil(request):
+    return render(request, "home/perfil.html")
