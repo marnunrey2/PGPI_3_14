64d9265df9786d37a6c15a1e8a97322a5f3171c3
Maria Nuñez <marnunrey2@alum.us.es>
2023-11-20 10:56:33 +0100

    feat: Cambiado orden

--- citas/urls.py
+++ citas/urls.py
@@ -3,8 +3,8 @@ from .views import ReservaView, ServiciosView, CalendarioView
 
 
 urlpatterns = [
-    path("reserva/", ReservaView.as_view(), name="reserva"),
-    path("servicios/", ServiciosView.as_view(), name="servicios"),
+    path("elige_servicios/", ServiciosView.as_view(), name="servicios"),
     path("calendario/", CalendarioView.as_view(), name="calendario"),
+    path("reserva/", ReservaView.as_view(), name="reserva"),
     # Add more URLs as needed
 ]
--- citas/views.py
+++ citas/views.py
@@ -4,42 +4,6 @@ from rest_framework.views import APIView
 from citas.models import Servicio, Especialista, Invitado
 
 
-class ReservaView(APIView):
-    def post(self, request):
-        form = ClientInfoForm(request.POST)
-        if form.is_valid():
-            # If the form is valid, create an Invitado instance
-            nombre = form.cleaned_data["nombre"]
-            email = form.cleaned_data["email"]
-            telefono = form.cleaned_data["telefono"]
-
-            invitado = Invitado.objects.create(
-                nombre=nombre, email=email, telefono=telefono
-            )
-
-            return render(
-                request,
-                "servicios.html",
-                {"invitado": invitado},
-            )
-        else:
-            form = ClientInfoForm()
-            msg = "Error de formulario"
-            return render(
-                request,
-                "reserva.html",
-                {"form": form, "msg": msg},
-            )
-
-    def get(self, request):
-        form = ClientInfoForm(None)
-        return render(
-            request,
-            "reserva.html",
-            {"form": form},
-        )
-
-
 class ServiciosView(APIView):
     def post(self, request):
         form = ServicioEspecialistaForm(request.POST)
@@ -70,7 +34,7 @@ class ServiciosView(APIView):
         form = ServicioEspecialistaForm(None)
         return render(
             request,
-            "servicios.html",
+            "elige_servicios.html",
             {"form": form, "servicios": servicios, "especialistas": especialistas},
         )
 
@@ -78,12 +42,50 @@ class ServiciosView(APIView):
 class CalendarioView(APIView):
     def post(self, request):
         form = request.data.get("form")
-        return redirect("/")
+        return render(
+            request,
+            "reserva.html",
+            {"form": form},
+        )
 
     def get(self, request):
         form = request.data.get("form")
         return render(
             request,
             "calendario.html",
+            {"form": form},
+        )
+
+
+class ReservaView(APIView):
+    def post(self, request):
+        form = ClientInfoForm(request.POST)
+        if form.is_valid():
+            # If the form is valid, create an Invitado instance
+            nombre = form.cleaned_data["nombre"]
+            email = form.cleaned_data["email"]
+            telefono = form.cleaned_data["telefono"]
+
+            invitado = Invitado.objects.create(
+                nombre=nombre, email=email, telefono=telefono
+            )
+
+            form.save()
+
+            return redirect("/")
+        else:
+            form = ClientInfoForm()
+            msg = "Error de formulario"
+            return render(
+                request,
+                "reserva.html",
+                {"form": form, "msg": msg},
+            )
+
+    def get(self, request):
+        form = ClientInfoForm(None)
+        return render(
+            request,
+            "reserva.html",
             {"form": form},
         )
