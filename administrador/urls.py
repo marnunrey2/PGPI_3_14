bb6e2683efed65ce2c91fa29ac0d3ebc570dfe51
auroranavas <navasaurora@outlook.com>
2023-11-26 00:28:11 +0100

    feat: admin ve y borra todos los modelos

--- administrador/urls.py
+++ administrador/urls.py
@@ -1,7 +1,51 @@
 from django.urls import path, include
-from .views import AdminCitaView
+from .views import (
+    AdminCitaView,
+    AdminServicioView,
+    servicio_delete,
+    especialista_delete,
+    AdminEspecialistaView,
+    AdminUsuarioView,
+    usuario_delete,
+    AdminInvitadoView,
+    invitado_delete,
+)
 
 urlpatterns = [
     path("admin_view/citas", AdminCitaView.as_view(), name="admin_cita"),
+    path("admin_view/servicios", AdminServicioView.as_view(), name="admin_servicio"),
+    path(
+        "servicios/<int:servicio_id>/delete/", servicio_delete, name="servicio_delete"
+    ),
+    path(
+        "admin_view/especialistas",
+        AdminEspecialistaView.as_view(),
+        name="admin_especialista",
+    ),
+    path(
+        "especialistas/<int:especialista_id>/delete/",
+        especialista_delete,
+        name="especialista_delete",
+    ),
+    path(
+        "admin_view/usuarios",
+        AdminUsuarioView.as_view(),
+        name="admin_usuario",
+    ),
+    path(
+        "usuarios/<int:usuario_id>/delete/",
+        usuario_delete,
+        name="usuario_delete",
+    ),
+    path(
+        "admin_view/invitados",
+        AdminInvitadoView.as_view(),
+        name="admin_invitado",
+    ),
+    path(
+        "invitados/<int:invitado_id>/delete/",
+        invitado_delete,
+        name="invitado_delete",
+    ),
     path("", include(("home.urls", "home"), namespace="home")),
 ]
--- administrador/views.py
+++ administrador/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework.views import APIView
-from citas.models import Cita
+from citas.models import Cita, Servicio, Especialista, Invitado
+from django.http import HttpResponseForbidden
 from django.contrib.auth.models import User
 
 
@@ -12,3 +13,83 @@ class AdminCitaView(APIView):
             return render(request, "admin_cita.html", context)
         else:
             return redirect("home:home")
+
+
+class AdminServicioView(APIView):
+    def get(self, request):
+        if request.user.is_staff:
+            servicios = Servicio.objects.all()
+            context = {"servicios": servicios}
+            return render(request, "admin_servicio.html", context)
+        else:
+            return redirect("home:home")
+
+
+def servicio_delete(request, servicio_id):
+    servicio = get_object_or_404(Servicio, pk=servicio_id)
+
+    if request.user.is_staff:
+        servicio.delete()
+        return redirect("/admin_view/servicios")
+    else:
+        return HttpResponseForbidden()
+
+
+class AdminEspecialistaView(APIView):
+    def get(self, request):
+        if request.user.is_staff:
+            especialistas = Especialista.objects.all()
+            context = {"especialistas": especialistas}
+            return render(request, "admin_especialista.html", context)
+        else:
+            return redirect("home:home")
+
+
+def especialista_delete(request, especialista_id):
+    especialista = get_object_or_404(Especialista, pk=especialista_id)
+
+    if request.user.is_staff:
+        especialista.delete()
+        return redirect("/admin_view/especialistas")
+    else:
+        return HttpResponseForbidden()
+
+
+class AdminUsuarioView(APIView):
+    def get(self, request):
+        if request.user.is_staff:
+            usuarios = User.objects.all()
+            context = {"usuarios": usuarios}
+            return render(request, "admin_usuario.html", context)
+        else:
+            return redirect("home:home")
+
+
+def usuario_delete(request, usuario_id):
+    usuario = get_object_or_404(User, pk=usuario_id)
+
+    if request.user.is_staff:
+        usuario.delete()
+        return redirect("/admin_view/usuarios")
+    else:
+        return HttpResponseForbidden()
+
+
+class AdminInvitadoView(APIView):
+    def get(self, request):
+        if request.user.is_staff:
+            invitados = Invitado.objects.all()
+            context = {"invitados": invitados}
+            return render(request, "admin_invitado.html", context)
+        else:
+            return redirect("home:home")
+
+
+def invitado_delete(request, invitado_id):
+    invitado = get_object_or_404(Invitado, pk=invitado_id)
+
+    if request.user.is_staff:
+        invitado.delete()
+        return redirect("/admin_view/invitados")
+    else:
+        return HttpResponseForbidden()
