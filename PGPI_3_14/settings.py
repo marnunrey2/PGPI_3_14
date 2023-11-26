3b0937dec8c2a1612257d1ce9c23b4a221cd602b
auroranavas <navasaurora@outlook.com>
2023-11-26 12:37:46 +0100

    fix: cambio de directorio de guardado de imagenes

--- PGPI_3_14/settings.py
+++ PGPI_3_14/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
