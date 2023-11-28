45c323bb07c0b0f613b62a1aa8a3a0a2b125e0ac
Maria Nuñez <marnunrey2@alum.us.es>
2023-11-28 19:45:19 +0100

    fix: errors and style

--- home/urls.py
+++ home/urls.py
@@ -13,14 +13,14 @@ app_name = "home"
 
 urlpatterns = [
     path("", HomeView, name="home"),
+    path("servicios/", servicios, name="servicios"),
+    path("especialistas/", especialistas, name="especialistas"),
     path(
         "about/",
         TemplateView.as_view(template_name="home/about.html"),
         name="about",
     ),
     path("perfil/", perfil, name="perfil"),
-    path("servicios/", servicios, name="servicios"),
-    path("especialistas/", especialistas, name="especialistas"),
-    path("perfil/update_profile/", update_profile, name="update_profile"),
-    path("perfil/update_password/", update_password, name="update_password"),
+    path("perfil/update_profile", update_profile, name="update_profile"),
+    path("perfil/update_password", update_password, name="update_password"),
 ]
