99c78c3946304c999a7bf1fc3f4ba2c6e65c1dfd
Sammy <samrodman@alum.us.es>
2023-11-28 13:04:31 +0100

    feature: aparecen fotos servicios con instancias

--- PGPI_3_14/settings.py
+++ PGPI_3_14/settings.py
@@ -132,3 +132,4 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MODULES = ["authentication", "citas", "home", "administrador"]
+# Media settings
--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 from .views import (
     HomeView,
@@ -22,3 +23,7 @@ urlpatterns = [
     path("especialistas/", especialistas, name="especialistas"),
     path("perfil/update_profile/", update_profile, name="update_profile"),
 ]
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
