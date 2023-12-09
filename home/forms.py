be65c0cba2593167803159917b36aad4da44edf8
Maria Nuñez <marnunrey2@alum.us.es>
2023-12-09 15:08:51 +0100

    feat: get precio del servicio

--- home/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class SearchForm(forms.Form):
-    servicio = forms.CharField(required=False)
-    especialista = forms.CharField(required=False)
-    max_precio = forms.DecimalField(
-        required=False, min_value=0, max_digits=10, decimal_places=2
-    )
